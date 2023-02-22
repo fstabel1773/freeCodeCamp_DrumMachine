30da8400cc4bcba2362ec4368f3f7d1f2300e2f6
Florian Stabel <f.stabel@gmx.de>
2023-02-22 13:11:17 +0100

    fix: functional version passing tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 import drumData from "./drumData"
-import DrumPad from "./DrumPad"
 
 
 function App() {
   const [drumPattern, setDrumPattern] = useState([])
   const keyDown = useRef(null)
+
+  const key = useRef(null)
+
+  useEffect(() => {
+    keyDown.current.focus()
+  }, [drumPattern])
 
   function handleKeyDown(event) {
     drumData.forEach(drum => {
@@ -15,32 +20,42 @@ function App() {
   }
 
   function handleDrum(desc) {
-    const matchingDrum = drumData.find(drum => drum.description === desc)
-    setDrumPattern(prevDrumPattern => [...prevDrumPattern, matchingDrum])
-    let audio = new Audio(matchingDrum.url)
-    audio.play()
+    setDrumPattern(prevDrumPattern => [...prevDrumPattern, desc])
+    key.current.children.forEach((drumPad) => {
+      if (drumPad.id === desc) {
+        drumPad.firstChild.play()
+      } 
+    })
   }
 
   const drumPads = drumData.map(drum => {
     return (
-    <DrumPad 
-      key={drum.id} 
-      drum={drum} 
-      drumPattern={drumPattern} 
-      handleDrum={handleDrum} />
+      <button
+        key={drum.id}
+        id={drum.description}
+        className="drum-pad"
+        onClick={() => handleDrum(drum.description)} >
+        <audio 
+          id={drum.id}
+          src={drum.url} 
+          className="clip"
+           >
+        </audio>
+        {drum.id}
+      </button>
     )
   })
 
   return (
     <div id="App-Wrapper" className="" tabIndex={0} ref={keyDown} onKeyDown={(event) => handleKeyDown(event)}>
       <div id="drum-machine" className="">
-        <div id="drumpad-grid">
+        <div id="drumpad-grid" ref={key}>
           {drumPads}
         </div>
         <div id="setting-grid">
           <div id="display">
            {drumPattern.length > 0
-            ? drumPattern[drumPattern.length - 1].description 
+            ? drumPattern[drumPattern.length - 1] 
             : " "}
           </div>
         </div>
